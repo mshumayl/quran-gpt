47742ffef0f84ed58dcd86f05330b7fad0084586
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-03-07 22:27:34 +0800

    Rerun Prisma migration

--- src/env.mjs
+++ src/env.mjs
@@ -6,6 +6,7 @@ import { z } from "zod";
  */
 const server = z.object({
   DATABASE_URL: z.string().url(),
+  DATABASE_DIRECT_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
@@ -41,6 +42,7 @@ const client = z.object({
  */
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_DIRECT_URL: process.env.DATABASE_DIRECT_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
