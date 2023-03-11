9d6a61ee66c5d7026711ec4ff1797c63841ac192
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-03-11 23:39:15 +0800

    Perform prisma migrations for storing verses
    - Create SurahMetadata and Verses tables in schema
    - Migrate prisma schema to supabase
    - Loaded data into SurahMetadata and Verses

---
+++ prisma/migrations/20230311125714_add_primary_keys_for_surah_metadata_and_verses/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "SurahMetadata" ADD CONSTRAINT "SurahMetadata_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Verses" ADD CONSTRAINT "Verses_pkey" PRIMARY KEY ("id");
