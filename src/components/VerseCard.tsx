fe2fcc3c79c827a951f14b06255cc56169d742c2
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-03-12 15:40:27 +0800

    Link to external references in VerseCard
    - Tafseer from www.alim.org
    - Asbab al-Nuzul from www.altafsir.com
    - Full Quran text from quran.com

--- src/components/VerseCard.tsx
+++ src/components/VerseCard.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import Link from 'next/link';
 import React, { useState, type FC } from 'react'
 import { api } from '~/utils/api';
 
@@ -25,6 +26,12 @@ const VerseCard: FC<VerseCardProps> = ({ surah, verse }) => {
       <div className="font-lateef text-3xl">{dbFetch.data?.verseText}</div>
       <br></br>
       <div className="font-zilla-slab-italic text-lg">{dbFetch.data?.verseTranslation}</div>
+      <div className="flex flex-col sm:flex-row mt-5 gap-2 sm:gap-6 justify-center items-center text-xs font-zilla-slab-italic text-slate-600 ">
+        <div className="">References</div>
+        <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.alim.org/quran/tafsir/ibn-kathir/surah/${surah}/${verse}/`}>Tafseer ibn Kathir</Link>
+        <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.altafsir.com/AsbabAlnuzol.asp?SoraName=${surah}&Ayah=${verse}&search=yes&img=A&LanguageID=2`}>Asbaab al-Nuzul</Link>
+        <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://quran.com/${surah}?startingVerse=${verse}`}>Full text</Link>
+      </div>
     </div>
   )
 }
