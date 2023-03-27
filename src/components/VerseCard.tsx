617a418a134411d3f3d2e4e0f0c223be8eb56e44
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-03-27 23:17:41 +0800

    Render metadata in verse/

--- src/components/VerseCard.tsx
+++ src/components/VerseCard.tsx
@@ -16,18 +16,30 @@ const VerseCard: FC<VerseCardProps> = ({ surah, verse, isDetailed }) => {
 
   //make tRPC calls to fetch surahName and verse here
   const dbFetch = api.db.fetchVerse.useQuery({surahNumber: surah.toString(), verseNumber: verse.toString()})
-  
+  let dbFetchDetails = null
   //query detailed metadata separately
   if (isDetailed) {
     //detailed call
-    const dbFetchDetails = null
+    dbFetchDetails = api.db.fetchDetails.useQuery({ surahNumber: surah.toString() })
+    console.log(dbFetchDetails.data)
   } 
   
 
 
   return ((dbFetch.data) ? (
     <div className="bg-slate-200 m-10 p-10 border border-dashed border-slate-400 rounded-xl w-full flex flex-col text-center shadow-xl">
-      <div className="font-zilla-slab-italic" key={`surahverse-${surah}_${verse}`}>— {dbFetch.data?.surahName}, {verse} —</div>
+      {
+      (isDetailed && dbFetchDetails) //Renders only in verse/ endpoint
+      ? (
+      <div className="w-max sm:w-max self-center px-5 sm:px-10 flex flex-row content-evenly align-middle justify-center gap-5 items-baseline mb-5 bg-slate-300 rounded-full border border-dashed border-slate-400">
+        <div className="text-slate-500 mb-2 font-lateef text-md" key={`translation_${surah}_${verse}`}>{dbFetchDetails.data?.surahEName}</div>
+        <div className="text-slate-500 font-lateef text-3xl" key={`arabictitle_${surah}_${verse}`}>{dbFetchDetails.data?.surahName}</div>
+        <div className="text-slate-500 mb-2 font-lateef text-md" key={`type_${surah}_${verse}`}>{dbFetchDetails.data?.surahType}</div>
+      </div>
+      ) 
+      : (<></>)
+      }
+      <div className="font-zilla-slab-italic" key={`surahverse_${surah}_${verse}`}>— {dbFetch.data?.surahName}, {verse} —</div>
       <br></br>
       <div className="font-lateef text-3xl">{dbFetch.data?.verseText}</div>
       <br></br>
@@ -38,7 +50,6 @@ const VerseCard: FC<VerseCardProps> = ({ surah, verse, isDetailed }) => {
         <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.altafsir.com/AsbabAlnuzol.asp?SoraName=${surah}&Ayah=${verse}&search=yes&img=A&LanguageID=2`}>Asbaab al-Nuzul</Link>
         <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://quran.com/${surah}?startingVerse=${verse}`}>Full text</Link>
       </div>
-      
     </div>
   ) : (<div className="animate-ping font-zilla-slab-italic text-xs h-max w-max text-slate-500 my-10 rounded-lg bg-slate-200 py-1 px-2">Fetching verses...</div>))
 }
