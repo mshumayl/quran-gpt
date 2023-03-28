10a8565b4443f6a1ac2b103a8b46ac7ec7a838c0
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-03-28 22:20:36 +0800

    Improve VerseCard dynamic styling
    - Animate on hover
    - Render references only in verse/
    - Reduce text contrast

--- src/components/VerseCard.tsx
+++ src/components/VerseCard.tsx
@@ -11,6 +11,8 @@ interface VerseCardProps {
   isDetailed?: boolean;
 }
 
+
+
 const VerseCard: FC<VerseCardProps> = ({ surah, verse, isDetailed }) => {
   // const [verseText, setVerseText] = useState<string>("")
 
@@ -24,33 +26,40 @@ const VerseCard: FC<VerseCardProps> = ({ surah, verse, isDetailed }) => {
     console.log(dbFetchDetails.data)
   } 
   
-
-
   return ((dbFetch.data) ? (
-    <div className="bg-slate-200 m-10 p-10 border border-dashed border-slate-400 rounded-xl w-full flex flex-col text-center shadow-xl">
-      {
-      (isDetailed && dbFetchDetails) //Renders only in verse/ endpoint
-      ? (
-      <div className="w-max sm:w-max self-center px-5 sm:px-10 flex flex-row content-evenly align-middle justify-center gap-5 items-baseline mb-5 bg-slate-300 rounded-full border border-dashed border-slate-400">
-        <div className="text-slate-500 mb-2 font-lateef text-md" key={`translation_${surah}_${verse}`}>{dbFetchDetails.data?.surahEName}</div>
-        <div className="text-slate-500 font-lateef text-3xl" key={`arabictitle_${surah}_${verse}`}>{dbFetchDetails.data?.surahName}</div>
-        <div className="text-slate-500 mb-2 font-lateef text-md" key={`type_${surah}_${verse}`}>{dbFetchDetails.data?.surahType}</div>
+    <>
+      <div className={`bg-slate-200 m-10 p-10 border border-dashed border-slate-400 rounded-xl w-full flex flex-col text-center shadow-xl transition-all ${(isDetailed) ? (""): ("hover:translate-x-1 hover:-translate-y-1 hover:shadow-2xl")}`}>
+        {
+          (isDetailed && dbFetchDetails) //Renders only in verse/ endpoint
+          ? (
+          <div className="w-max sm:w-max self-center px-5 sm:px-10 flex flex-row content-evenly align-middle justify-center gap-5 items-baseline mb-5 bg-slate-300 rounded-full border border-dashed border-slate-400 shadow-inner">
+            <div className="text-slate-500 mb-2 font-lateef text-md" key={`translation_${surah}_${verse}`}>{dbFetchDetails.data?.surahEName}</div>
+            <div className="text-slate-500 font-lateef text-3xl" key={`arabictitle_${surah}_${verse}`}>{dbFetchDetails.data?.surahName}</div>
+            <div className="text-slate-500 mb-2 font-lateef text-md" key={`type_${surah}_${verse}`}>{dbFetchDetails.data?.surahType}</div>
+          </div>
+            ) 
+          : (<></>)
+        }
+        <div className="font-zilla-slab-italic text-slate-500" key={`surahverse_${surah}_${verse}`}>— {dbFetch.data?.surahName}, {verse} —</div>
+        <br></br>
+        <div className="font-lateef text-3xl text-slate-600">{dbFetch.data?.verseText}</div>
+        <br></br>
+        <div className="font-zilla-slab-italic text-lg text-slate-500">{dbFetch.data?.verseTranslation}</div>
+
+        {
+          (isDetailed) //Renders only in verse/ endpoint
+          ? (
+            <div className="flex flex-col sm:flex-row mt-5 gap-2 sm:gap-6 justify-center items-center text-xs font-zilla-slab-italic text-slate-600 ">
+              <div className="">References</div>
+              <Link target="_blank" rel="noopener" passHref className="shadow-inner py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.alim.org/quran/tafsir/ibn-kathir/surah/${surah}/${verse}/`}>Tafseer ibn Kathir</Link>
+              <Link target="_blank" rel="noopener" passHref className="shadow-inner py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.altafsir.com/AsbabAlnuzol.asp?SoraName=${surah}&Ayah=${verse}&search=yes&img=A&LanguageID=2`}>Asbaab al-Nuzul</Link>
+              <Link target="_blank" rel="noopener" passHref className="shadow-inner py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://quran.com/${surah}?startingVerse=${verse}`}>Full text</Link>
+            </div>
+          ) 
+          : (<></>)
+        }
       </div>
-      ) 
-      : (<></>)
-      }
-      <div className="font-zilla-slab-italic" key={`surahverse_${surah}_${verse}`}>— {dbFetch.data?.surahName}, {verse} —</div>
-      <br></br>
-      <div className="font-lateef text-3xl">{dbFetch.data?.verseText}</div>
-      <br></br>
-      <div className="font-zilla-slab-italic text-lg">{dbFetch.data?.verseTranslation}</div>
-      <div className="flex flex-col sm:flex-row mt-5 gap-2 sm:gap-6 justify-center items-center text-xs font-zilla-slab-italic text-slate-600 ">
-        <div className="">References</div>
-        <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.alim.org/quran/tafsir/ibn-kathir/surah/${surah}/${verse}/`}>Tafseer ibn Kathir</Link>
-        <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://www.altafsir.com/AsbabAlnuzol.asp?SoraName=${surah}&Ayah=${verse}&search=yes&img=A&LanguageID=2`}>Asbaab al-Nuzul</Link>
-        <Link target="_blank" rel="noopener" passHref className="py-2 px-3 border border-dashed border-slate-400 w-max bg-slate-50 h-max hover:bg-slate-100" href={`https://quran.com/${surah}?startingVerse=${verse}`}>Full text</Link>
-      </div>
-    </div>
+    </>
   ) : (<div className="animate-ping font-zilla-slab-italic text-xs h-max w-max text-slate-500 my-10 rounded-lg bg-slate-200 py-1 px-2">Fetching verses...</div>))
 }
 
