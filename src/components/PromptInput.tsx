425bf572aaa564233bf108e0ad505836f84e9e29
mshumayl <mshumayl@gmail.com>
2023-04-02 13:13:23 +0800

    Improve aesthetics
    - Add inner shadow for current mobile page
    - Add cycling placeholder text as prompt examples

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -24,7 +24,8 @@ const PromptInput: FC = ({  }) => {
 
   const [inputValue, setInputValue] = useState<string>("");
   const [inputLength, setInputLength] = useState<number>(0);
-
+  const [placeholderIndex, setPlaceholderIndex] = useState<number>(0);
+  const [displayLoader, setDisplayLoader] = useState<boolean>(false);
   const [aiResponse, setAiResponse] = useState<responseType>(() => {
     // Assign cachedResponse if useState is being run on client-side
     const cachedResponse = (
@@ -35,7 +36,6 @@ const PromptInput: FC = ({  }) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return cachedResponse;
   });
-  const [displayLoader, setDisplayLoader] = useState<boolean>(false);
   
   const maxInputLength = 140;
 
@@ -60,6 +60,41 @@ const PromptInput: FC = ({  }) => {
     setAiResponse(defaultResponse);
   }
 
+  const placeholder = [
+    "Ruling on smoking cigarettes", 
+    "Importance of striving towards knowledge", 
+    "அறிவை நோக்கிச் செல்வதன் முக்கியத்துவம்",
+    "Hukum berpuasa di bulan Ramadhan", 
+    "Abraham's sacrifice",
+    "Story of Yusuf and his brothers", 
+    "Di manakah bahtera Nabi Nuh mendarat",
+    "Birth of Jesus",
+    "Nascita di Gesù",
+    "Story of David vs Goliath",
+    "Kebobrokan yang dilakukan oleh Samiri",
+    "قصة موسى ضد فرعون",
+    "Politics in Islam",
+    "அரசியல் மற்றும் தலைமைத்துவம்",
+    "关于禁食的裁决",
+    "Kebejatan sosial",
+    "善待孤儿"
+  ]
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPlaceholderIndex((prevState) => {
+        const newIdx = Math.floor(Math.random() * (placeholder.length))
+      
+        if (newIdx !== prevState) {
+          return newIdx
+        } else {
+          return placeholder.length % newIdx
+        }
+
+      })
+    }, 3000)
+    return (() => clearInterval(interval))
+  })
 
   useEffect(() => {
     setInputLength(inputValue.length);
@@ -73,14 +108,14 @@ const PromptInput: FC = ({  }) => {
   return (
     <>
         <form onSubmit={handleSubmit} id="promptForm" name="promptForm" className="flex flex-col justify-center">
-              <label htmlFor="promptInput" className="flex mb-1 mx-1 font-zilla-slab-italic">Prompt</label>
+              <label htmlFor="promptInput" className="flex mb-1 mx-1 font-zilla-slab-italic">Search</label>
               <textarea
               form="promptForm"
               rows={2}
               cols={50}
               id="promptInput"
-              className="p-2 flex w-full h-24 rounded-lg text-sm border border-dashed border-slate-400"
-              placeholder="Query the Quran"
+              className="transition-all p-2 flex w-full h-24 rounded-lg text-sm border border-dashed border-slate-400 shadow-inner"
+              placeholder={placeholder[placeholderIndex]}
               maxLength={maxInputLength}
               onChange={(event) => {
                 setInputValue(event.target.value);
