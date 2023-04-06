bee3f68733f87b3faea08dbb90f7715ff89765f0
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-04-07 01:20:54 +0800

    Store textarea value in state

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,5 +1,5 @@
 import { useSession } from 'next-auth/react';
-import React, { type FC } from 'react'
+import React, { useState, type FC } from 'react'
 
 interface NotesProps {
     notes?: string;
@@ -8,12 +8,23 @@ interface NotesProps {
 const Notes: FC<NotesProps> = ({ notes }) => {
   const { data: sessionData } = useSession();
 
+  const [ noteValue, setNoteValue ] = useState("");
+
+  
+
   return (
-    <>
-        <div>
-            Notes
+    <div className="w-screen px-20">
+        <div className="w-full p-10 bg-white rounded-xl shadow-inner border border-dashed border-slate-400">
+            <div>
+                Notes
+            </div>
+            <textarea className="appearance-none resize-none w-full min-h-full" onChange={(event) => {
+                setNoteValue(event.target.value);
+              }}>
+            </textarea>
+            <div className="bg-slate-200">{noteValue}</div>
         </div>
-    </>
+    </div>
   )
 }
 
