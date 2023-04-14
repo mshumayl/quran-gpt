f1e776907128498b8f9c45307e073b02618683ae
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-04-14 21:57:21 +0800

    AI generate button on note input field

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -24,6 +24,15 @@ const SubmitNoteButton: FC = () => {
   )
 }
 
+const AIGenerateNoteButton: FC = () => {
+  return (
+    <button className="m-1 w-max py-1 px-2 text-white bg-purple-300 hover:bg-purple-400 border border-dashed border-purple-600 mr-2 transition-all grid grid-cols-2 justify-items-center items-center rounded-md" type="submit">
+        <div className="font-righteous">AI</div>
+        <svg className="w-5 h-5 stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"/></svg>
+    </button>
+  )
+}
+
 const Notes: FC<NotesProps> = ({ userId, verseId }) => {
 
   const [ savedNoteValue, setSavedNoteValue ] = useState<savedNoteType>([{}]);
@@ -150,9 +159,7 @@ const Notes: FC<NotesProps> = ({ userId, verseId }) => {
         setSavedNoteValue([{}])
       }
     }
-
     void getNotesFetcher();
-
   }
 
   return (
@@ -166,6 +173,10 @@ const Notes: FC<NotesProps> = ({ userId, verseId }) => {
                   setNewNoteValue(event.target.value);
                 }}>
               </textarea>
+              <div className="content-end h-7 grid justify-items-end">
+                {(newNoteValue.length === 0) 
+                && (<AIGenerateNoteButton/>)}
+              </div>
           </div>
           <div className="h-7 grid justify-items-end">
               {(newNoteValue.length > 0) 
