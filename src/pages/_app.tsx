5bb0bb3e425ac7f8ec0003eddee477bd90628fa6
mshumayl <mshumayl@gmail.com>
2023-04-22 01:00:57 +0800

    Integrate with AWS Amplify, remove Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { Analytics } from '@vercel/analytics/react';
 
 import { api } from "~/utils/api";
 
@@ -14,7 +13,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <Analytics />
     </SessionProvider>
   );
 };
