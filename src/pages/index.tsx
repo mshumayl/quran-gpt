5d11ae8cfdc47a278dfa1fa8bcfb9c3136ac5367
mshumayl <mshumayl@gmail.com>
2023-04-22 02:08:07 +0800

    REBUILD: Fix typo in amplify.yml

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -103,13 +103,6 @@ const Home: NextPage = () => {
             </div>
 
           </div>
-
-          {/* <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
-          </div> */}
         </div>
       </main>
     </>
